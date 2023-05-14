37f7747677e64a16071a8d95d5adf55b0fee4eb5
Mmenyie <kekanem72@gmail.com>
2023-05-14 16:28:27 -0700

    print_matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        return
+    if len(matrix) == 0:
+        for integer in row:
+            print(str.format("{0}", integer), end=" ")
+        print()
