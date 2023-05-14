838f18e53cb411d3a75407d80283e1c4c87e101b
Mmenyie <kekanem72@gmail.com>
2023-05-14 16:09:36 -0700

    5-no

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for alphabet in my_string:
+        if alphabet != 'c' and alphabet != 'C':
+            new_string += alphabet
+    return new_string
