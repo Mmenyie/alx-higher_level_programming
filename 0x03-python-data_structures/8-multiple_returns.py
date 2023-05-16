ce4eb8e2e8b00c020ed4384cd5468904a2e8b4b5
Mmenyie <kekanem72@gmail.com>
2023-05-16 03:43:08 -0700

    multiple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        new = (0, None)
+        return new
+    else:
+        new = (length, sentence[0:1])
+        return new
