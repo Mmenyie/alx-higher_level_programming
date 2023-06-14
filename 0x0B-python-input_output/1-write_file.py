b9da3e5b53cb55ee9ec022b5fd80a6154c7836f9
Mmenyie <kekanem72@gmail.com>
2023-06-14 10:50:52 -0700

    Python_input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" contains the number_of_lines function """
+"""
+Contains the function "wrtie_file"
+"""
 
 
-def number_of_lines(filename=""):
-    """ returns the number of lines in a file """
-    count = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            count += 1
-    return count
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
